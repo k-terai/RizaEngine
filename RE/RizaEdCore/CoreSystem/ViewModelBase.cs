d39fc99b8d38381d2f66ed57bb7746b6bcac3671
k-terai <chibi201028@gmail.com>
2022-11-30 04:35:34 +0900

    fix: using compile error

--- RE/RizaEdCore/CoreSystem/ViewModelBase.cs
+++ RE/RizaEdCore/CoreSystem/ViewModelBase.cs
@@ -3,7 +3,9 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace RizaEdCore.CoreSystem
