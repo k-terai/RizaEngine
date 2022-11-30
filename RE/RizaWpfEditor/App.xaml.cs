6436ed5cccacbf6065b4c5d013c1d4357a322510
k-terai <chibi201028@gmail.com>
2022-12-01 04:27:31 +0900

    Rename editor project

--- RE/RizaWpfEditor/App.xaml.cs
+++ RE/RizaWpfEditor/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace RizaSandbox
+namespace RizaWpfEditor
 {
     /// <summary>
     /// Interaction logic for App.xaml
