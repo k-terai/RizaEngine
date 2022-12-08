e9340aff2fbea6f6d7458dd57d4f6e7050b733bc
k-terai <chibi201028@gmail.com>
2022-12-09 04:25:04 +0900

    Add EngineUnitTest project

---
+++ RE/EngineUnitTest/EngineUnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EngineUnitTest
+{
+	TEST_CLASS(EngineUnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
